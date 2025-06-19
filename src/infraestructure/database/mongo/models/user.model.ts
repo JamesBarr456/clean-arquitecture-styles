ec816a0f492cfd0bbeaea45e58a64c3eb0bc9c77
Emmanuel <barretosantiago96@hotmail.com>
2025-06-19 10:21:33 -0300

    feature: add user.model mongoose

---
+++ src/infraestructure/database/mongo/models/user.model.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: 
+        {
+            type: String,
+            required: true  
+    },
+    dni: {
+        type: String,
+        default: ""
+    },
+    number_phone: {
+        type: String,
+        default: ""
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+
+    },
+    status: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+      },
+      created_at: {
+        type: Date,
+        default: Date.now,
+      },
+      updated_at: {
+        type: Date,
+        default: Date.now,
+      },
+})
+
+export const UserModel = mongoose.model("User", userSchema)
